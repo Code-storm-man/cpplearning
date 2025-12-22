141f7e9060e998de88e375bb63c4519caf3f3b2e
zxY9000P <zxcocalabstu@outlook.com>
2025-12-22 21:58:09 +0800

    新增课程学习switch

---
+++ test_for/test_for.cpp
@@ -0,0 +1,30 @@
+﻿// test_for.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	for (int i = 0; /* 初始化 */i < 10/* 条件 */; i++ /* 迭代 */)
+    {
+        // cout << "Hello, World! " << i << endl;
+        // true代码块
+		cout <<"i = "<< i << endl;
+    }
+
+    // contitue的使用
+    // “循环不退出，下面代码此次不执行，循环进入下一轮”
+    // break
+    // 跳出当前所在层的循环
+    for (int i = 0; i < 100; i++)
+    {
+        if (i % 2 == 0)
+            continue;
+		cout << i << " ";
+        if (i > 50)
+            break;
+    }
+    cout << endl;
+}
---
+++ test_switch/test_switch.cpp
@@ -0,0 +1,59 @@
+﻿// test_switch.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+using namespace std;
+
+enum class STATUS
+{
+    PLAY,
+    STOP,
+    PAUSE
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    int x{ 2 };
+    cin >> x;
+
+    // switch用于整型
+    switch (x) // 判断x值满足哪个条件case, 直接跳转到相应地条件代码
+               // 运行到switch结束或者break
+    {
+    case 0:
+        cout << "case 0\n"; 
+        break;
+    case 1:
+    {
+		int y{ 1 };  // 如果需要在case中定义变量, 必须加大括号
+        cout << "begin 1" << endl;
+        cout << "case 1\n"; 
+        break;
+    }
+    case 2:
+        cout << "case 2\n"; 
+        break;
+    default:
+        cout << "default\n"; 
+        break;
+    }
+
+    // switch用于枚举类型
+    STATUS status{ STATUS::PLAY};
+    switch (status)
+    {
+        case STATUS::PLAY:
+            cout << "status: PLAY\n";
+		    break;
+        case STATUS::STOP:
+			cout << "status: STOP\n";
+            break;
+		case STATUS::PAUSE:
+			cout << "status: PAUSE\n";
+            break;
+    }
+
+}
